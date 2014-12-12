a6c96c2315916e033290714d0a321f0e8a3fc707
Dave W <drait@nebachadnezzar>
2014-12-11 17:01:20 -0800

    update bt override to only a specific string

--- xposedMTC/build/generated/source/r/debug/com/maxmpz/powerampapilib/R.java
+++ xposedMTC/build/generated/source/r/debug/com/maxmpz/powerampapilib/R.java
@@ -9,8 +9,8 @@ package com.maxmpz.powerampapilib;
 public final class R {
 	public static final class string {
 		public static final int app_name = 0x7f060000;
-		public static final int unknown = 0x7f06002e;
-		public static final int unknown_album_name = 0x7f06002f;
-		public static final int unknown_artist_name = 0x7f060030;
+		public static final int unknown = 0x7f060030;
+		public static final int unknown_album_name = 0x7f060031;
+		public static final int unknown_artist_name = 0x7f060032;
 	}
 }
